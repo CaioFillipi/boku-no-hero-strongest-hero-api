0b55fda294c4e5ade443577d91dbde2f82c56dcb
CaioFillipi <caio.fillipi@outlook.com>
2021-09-11 12:08:41 -0300

    First commit

---
+++ src/main/java/br/com/bokunoheroapi/demo/BokuNoHeroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bokunoheroapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokuNoHeroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokuNoHeroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bokunoheroapi/demo/BokuNoHeroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bokunoheroapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokuNoHeroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
